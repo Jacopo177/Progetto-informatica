8ee7d705b369779fbc0c2badb7261b1d67898273
Jacopo Natale <j.natale2@studenti.unipi.it>
2026-07-30 14:45:04 +0200

    codice task3 revisitato

--- Task3-progetto.cpp
+++ Task3-progetto.cpp
@@ -22,9 +22,9 @@ double u0(double x, double y) {
 }
 
 int main() {
-    std::cout << "--- TASK 3: Costruzione Matrice A e Vettore RHS ---" << std::endl;
+    std::cout << "TASK 3: Costruzione Matrice A e Vettore RHS" << std::endl;
 
-    // 1. Scelta dell'ordinamento da parte dell'utente
+    // 1. Scelta dell'ordinamento
     int scelta_ordine;
     std::cout << "Quale ordinamento vuoi usare?\n";
     std::cout << "0 = Ordinamento Naturale (da coords.txt)\n";
@@ -32,10 +32,10 @@ int main() {
     std::cout << "Scelta: ";
     std::cin >> scelta_ordine;
 
-    // 2. Lettura dei nodi da coords.txt usando il percorso assoluto fornito
+    // 2. Lettura dei nodi da coords.txt
     std::ifstream file_coords("coords.txt");
     if (!file_coords) {
-        std::cerr << "Errore: file coords.txt non trovato nel percorso specificato!" << std::endl;
+        std::cerr << "file coords.txt non trovato" << std::endl;
         return 1;
     }
 
@@ -50,22 +50,22 @@ int main() {
     int N = std::sqrt(num_nodi); // Risaliamo a N sapendo che i nodi sono N^2
     double h = 1.0 / (N + 1);
 
-    // AGGIORNAMENTO: Valore di kappa specificato dal progetto
+    //Valore di kappa specificato dal progetto
     double kappa = 0.01;
     double coeff = kappa / (h * h); // Il fattore k/h^2
 
-    // 3. Creazione del dizionario di traduzione (vecchio ID -> nuovo ID)
+    // 3. Creazione del dizionario di traduzione (old ID -> new ID)
     std::vector<int> old_to_new(num_nodi);
     if (scelta_ordine == 1) {
-        // Lettura da ordering.txt usando il percorso assoluto fornito
+        // Lettura da ordering.txt
         std::ifstream file_ordering("ordering.txt");
         if (!file_ordering) {
-            std::cerr << "Errore: ordering.txt non trovato nel percorso specificato!" << std::endl;
+            std::cerr << "ordering.txt non trovato" << std::endl;
             return 1;
         }
-        int nuovo_id, vecchio_id;
-        while (file_ordering >> nuovo_id >> vecchio_id) {
-            old_to_new[vecchio_id] = nuovo_id;
+        int new_id, old_id;
+        while (file_ordering >> new_id >> old_id) {
+            old_to_new[old_id] = new_id;
         }
         file_ordering.close();
     }
@@ -90,39 +90,39 @@ int main() {
         // Diagonale principale: 4 * (k/h^2)
         file_A << new_id << " " << new_id << " " << 4.0 * coeff << "\n";
 
-        // CONTROLLO DEI VICINI (La "stella" a 5 punti)
+        // Controllo dei vicini
 
-        // Vicino SINISTRO (i-1)
+        // Vicino sinistro (i-1)
         if (n.i > 1) {
-            int vicino_old_id = (n.j - 1) * N + (n.i - 2);
-            file_A << new_id << " " << old_to_new[vicino_old_id] << " " << -coeff << "\n";
+            int vicino_vecchio_id = (n.j - 1) * N + (n.i - 2);
+            file_A << new_id << " " << old_to_new[vicino_vecchio_id] << " " << -coeff << "\n";
         }
         else {
             rhs_val += coeff * u0(n.x - h, n.y);
         }
 
-        // Vicino DESTRO (i+1)
+        // Vicino destro (i+1)
         if (n.i < N) {
-            int vicino_old_id = (n.j - 1) * N + n.i;
-            file_A << new_id << " " << old_to_new[vicino_old_id] << " " << -coeff << "\n";
+            int vicino_vecchio_id = (n.j - 1) * N + n.i;
+            file_A << new_id << " " << old_to_new[vicino_vecchio_id] << " " << -coeff << "\n";
         }
         else {
             rhs_val += coeff * u0(n.x + h, n.y);
         }
 
-        // Vicino SOTTO (j-1)
+        // Vicino sotto (j-1)
         if (n.j > 1) {
-            int vicino_old_id = (n.j - 2) * N + (n.i - 1);
-            file_A << new_id << " " << old_to_new[vicino_old_id] << " " << -coeff << "\n";
+            int vicino_vecchio_id = (n.j - 2) * N + (n.i - 1);
+            file_A << new_id << " " << old_to_new[vicino_vecchio_id] << " " << -coeff << "\n";
         }
         else {
             rhs_val += coeff * u0(n.x, n.y - h);
         }
 
-        // Vicino SOPRA (j+1)
+        // Vicino sopra (j+1)
         if (n.j < N) {
-            int vicino_old_id = n.j * N + (n.i - 1);
-            file_A << new_id << " " << old_to_new[vicino_old_id] << " " << -coeff << "\n";
+            int vicino_vecchio_id = n.j * N + (n.i - 1);
+            file_A << new_id << " " << old_to_new[vicino_vecchio_id] << " " << -coeff << "\n";
         }
         else {
             rhs_val += coeff * u0(n.x, n.y + h);
@@ -141,6 +141,6 @@ int main() {
     }
     file_rhs.close();
 
-    std::cout << "Successo! File A.txt e rhs.txt generati correttamente." << std::endl;
+    std::cout << "File A.txt e rhs.txt generati" << std::endl;
     return 0;
 }
